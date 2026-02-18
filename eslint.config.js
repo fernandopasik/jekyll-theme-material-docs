a266c8b1fa8a9f2f2247afa5b9d32085d0cd1a09
Fernando Pasik <fernando@pasik.com.ar>
2026-02-18 10:52:52 +0000

    style: ignore build files

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import prettier from 'eslint-config-prettier';
 import ymlPlugin from 'eslint-plugin-yml';
 
 export default [
+  { ignores: ['_site', '.jekyll-cache', 'vendor'] },
   ...ymlPlugin.configs['flat/recommended'],
   ...ymlPlugin.configs['flat/prettier'],
   prettier,

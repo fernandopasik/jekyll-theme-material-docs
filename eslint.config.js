c7fe8e327a3b5fa816bd58702bab32540198e489
Fernando Pasik <fernando@pasik.com.ar>
2026-04-05 16:03:52 +0000

    style: update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import prettier from 'eslint-config-prettier';
-import ymlPlugin from 'eslint-plugin-yml';
+import { configs as ymlConfigs } from 'eslint-plugin-yml';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   { ignores: ['_site', '.jekyll-cache', 'vendor'] },
-  ...ymlPlugin.configs['flat/recommended'],
-  ...ymlPlugin.configs['flat/prettier'],
+  {
+    extends: [ymlConfigs.standard, ymlConfigs.prettier],
+    files: ['*.yaml', '*.yml'],
+  },
   prettier,
 ]);

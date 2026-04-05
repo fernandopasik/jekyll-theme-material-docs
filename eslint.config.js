76d878c01c7fb0bc0adfb021f3c6d0705ec37edb
Fernando Pasik <fernando@pasik.com.ar>
2026-04-05 16:04:54 +0000

    style: add plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,15 @@
+import markdown from '@eslint/markdown';
 import prettier from 'eslint-config-prettier';
 import { configs as ymlConfigs } from 'eslint-plugin-yml';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   { ignores: ['_site', '.jekyll-cache', 'vendor'] },
+  {
+    extends: [markdown.configs.recommended, markdown.configs.processor],
+    files: ['**/*.md'],
+    language: 'markdown/gfm',
+  },
   {
     extends: [ymlConfigs.standard, ymlConfigs.prettier],
     files: ['*.yaml', '*.yml'],
